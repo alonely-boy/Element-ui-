aceaa00ac3eef56443a395a45a0a2ac611ec2c7b
江南土拨鼠 <6527904+LuDun123@user.noreply.gitee.com>
2020-03-22 13:01:56 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';//忘了这个
+import "@/assets/fonts/style.css";/*引入字体样式 */
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/';//网络接口
+// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'              //本地接口
+Vue.prototype.$http = axios;
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import { from } from 'core-js/fn/array'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect:'/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path:'/home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next) =>{
+  //访问登录页，放行
+  if(to.path === '/login') return next();
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //没有token，跳转登录页
+  if(!tokenStr) return next('/login')
+  //last
+  next()
 })
 
 export default router
